9cc770174bf528413d0fefed28e49bb436cc1451
Naval <navalnovel@gmail.com>
2016-06-18 22:31:12 +0530

    upgraded kue-enhancer to use promises (archiejs 0.2 uses es6 promises)

--- test/kue_enhancer_test.js
+++ test/kue_enhancer_test.js
@@ -5,7 +5,6 @@ require('./../lib/util/inherit.js');
 var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
-var async = require('async');
 var resolve = require('path').resolve;
 
 var ServiceObj1 = require('./kueTestObj1'); // object
@@ -25,18 +24,14 @@ var redisConfig = {
 
 describe('Kue Enhancer Testcases:', function(){
     
-    before(function(){
+    before(function(done){
         kueEnhancer = new KueEnhancer();
-    });
-
-    afterEach(function(){
-        kueEnhancer.closeClient();
+        done();
     });
 
     it('#makes rpc calls', function(done){
         // create producer - consumer
         var count = 0;
-        var serviceInstances;
         var basedir = resolve(__dirname, '..');
         var config = {
             packagePath: 'test',
@@ -55,64 +50,49 @@ describe('Kue Enhancer Testcases:', function(){
         configClient.packageRole = 'client';
         configServer.packageRole = 'server';
 
-        async.waterfall([
-            function(cb){
-                // check resolveConfig client
-                kueEnhancer.resolveConfig(configClient, basedir);
-                configClient.provides.length.should.equal(2);
-                configClient.enhancers.should.have.property('Obj1');
-                configClient.enhancers.should.have.property('Obj2');
-                cb();
-            },
-            function(cb){
-                // check resolveConfig server
-                kueEnhancer.resolveConfig(configServer, basedir);
-                configServer.provides.length.should.equal(0);
-                cb();
-            },
-            function(cb){
-                // check resolveConfig setupPlugin
-                kueEnhancer.setupPlugin(configServer, {},
-                    function(err, services){
-                        serviceInstances = services;
-                        cb(); 
-                    }
-                );
-            },
-            function(cb){
-                // check setupPlugin client
-                kueEnhancer.setupPlugin(configClient, {}, 
-                    function(err, serviceMap){
-                        if(err) {
-                            throw err;
-                        }
+        var server = {};
 
-                        serviceMap.should.have.property('Obj1');
-                        serviceMap.should.have.property('Obj2');
+        Promise
+        .resolve()
+        .then(() => {
+            //console.log('setup server');
+            kueEnhancer.resolveConfig(configServer, basedir);
+            configServer.provides.length.should.equal(0);
+            return kueEnhancer.setupPlugin(configServer, {});
+        })
+        .then((_server) => {
+            server = _server;
 
-                        serviceMap.Obj1.func1( function(){ count++; } );
-                        serviceMap.Obj1.func2( function(){ count++; } );
-                        serviceMap.Obj1.func3( function(){ count++; } );
+            //console.log('setup client');
+            kueEnhancer.resolveConfig(configClient, basedir);
+            configClient.provides.length.should.equal(2);
+            configClient.enhancers.should.have.property('Obj1');
+            configClient.enhancers.should.have.property('Obj2');
+            return kueEnhancer.setupPlugin(configClient, {});
+        })
+        .then((client) => {
+            client.should.have.property('Obj1');
+            client.should.have.property('Obj2');
+            client.Obj1.func1( function(){ count++; } );
+            client.Obj1.func2( function(){ count++; } );
+            client.Obj1.func3( function(){ count++; } );
+            // wait for a bit - let all callbacks above arrive
+            return new Promise((resolve) => setTimeout(resolve, 1000));
+         })
+        .then(() => {
+            server.Obj1.func1_count.should.equal(1);
+            server.Obj1.func2_count.should.equal(1);
+            server.Obj1.func3_count.should.equal(1);
+            count.should.equal(3);
+            //console.log(server);
+         })
+         .then(kueEnhancer.closeClient.call(kueEnhancer))
+         .then(done)
+         .catch((err) => {
+            console.error("ALERT! Turn on DEBUG=true in lib/micro_base_enhancer.js for testing RPC calls.");
+            done(err);
+         })
 
-                        setTimeout(cb, 1000);
-                    }
-                );
-            },
-            function(cb){
-                var isNotEmpty = Object.keys(serviceInstances).length > 0;
-                if(isNotEmpty){
-                    console.log( serviceInstances.Obj1 );
-                    serviceInstances.Obj1.func1_count.should.equal(1);
-                    serviceInstances.Obj1.func2_count.should.equal(1);
-                    serviceInstances.Obj1.func3_count.should.equal(1);
-                }else{
-                    console.error("ALERT! Turn on DEBUG=true in core/microservice.js for testing RPC calls.");
-                }
-                count.should.equal(3);
-                cb();
-            }],
-            done
-        );
-    });
+    }).timeout(4000);
 
 }); 
